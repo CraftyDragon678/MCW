770be93510e7b24832aede25814cc485764637bc
CraftyDragon678 <craftydragon678@gmail.com>
2020-01-16 20:49:21 +0900

    initial commit

---
+++ src/main/java/com/cragon/mcw/MCW.java
@@ -0,0 +1,17 @@
+package com.cragon.mcw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCW extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
