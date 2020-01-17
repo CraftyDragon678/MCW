766a899e6b755e77b3376a8f69834d5139b6066b
CraftyDragon678 <craftydragon678@gmail.com>
2020-01-17 20:22:50 +0900

    skillbase to kotlin

--- src/main/kotlin/com/cragon/mcw/manager/SkillBase.kt
+++ src/main/kotlin/com/cragon/mcw/manager/SkillBase.kt
@@ -1,24 +1,34 @@
 package com.cragon.mcw.manager
 
+import com.cragon.mcw.MCW
 import org.bukkit.Material
 import org.bukkit.event.Event
 import org.bukkit.inventory.ItemStack
-import java.util.*
 
 abstract class SkillBase {
+
     var skillName: String? = null
     var manual: List<String>? = null
     var skillItem: ItemStack? = null
 
-    abstract fun execute(e: Event?, idx: Int)
+    abstract fun execute(_e: Event?, idx: Int)
 
-    protected fun initSkill(skillName: String?, item: Material?, vararg manual: String?) {
+    protected fun initSkill(skillName: String, item: Material, vararg manual: String) {
         this.skillName = skillName
-        this.manual = Arrays.asList<String>(*manual)
-        skillItem = ItemStack(item!!)
+        this.manual = manual.asList()
+        skillItem = ItemStack(item)
         val im = skillItem!!.itemMeta!!
         im.lore = this.manual
         im.setDisplayName(skillName)
         skillItem!!.itemMeta = im
+    }
+
+    companion object {
+        lateinit var main: MCW
+
+        fun initSkillBase(main: MCW) {
+            SkillBase.main = main
+        }
+
     }
 }
