d408922831ffd7747dbcfe10a173a7c69c6e152f
CraftyDragon678 <craftydragon678@gmail.com>
2020-01-17 20:24:43 +0900

    add SkillManager

--- src/main/kotlin/com/cragon/mcw/manager/SkillManager.kt
+++ src/main/kotlin/com/cragon/mcw/manager/SkillManager.kt
@@ -2,6 +2,9 @@ package com.cragon.mcw.manager
 
 import com.cragon.mcw.skill.Skill1
 
-object SkillManager {
-    val skill1 = Skill1()
+class SkillManager {
+    companion object {
+
+        val skill1 = Skill1()
+    }
 }
