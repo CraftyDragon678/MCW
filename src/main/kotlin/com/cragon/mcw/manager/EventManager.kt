9b2c957a2518b6b96c99904fa170292a709a56e3
CraftyDragon678 <craftydragon678@gmail.com>
2020-01-17 20:23:33 +0900

    edit EventManager
    add onInteract, onExplosionPrime event
    for skill1
    
    todo: seperate right click event and left click event

--- src/main/kotlin/com/cragon/mcw/manager/EventManager.kt
+++ src/main/kotlin/com/cragon/mcw/manager/EventManager.kt
@@ -1,23 +1,32 @@
 package com.cragon.mcw.manager
 
 import com.cragon.mcw.helper.EventData
+import org.bukkit.Bukkit
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.ExplosionPrimeEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import java.util.*
 import java.util.function.Consumer
 
 class EventManager : Listener {
-    fun eventExecuter(ed: ArrayList<EventData>, e: Event?) {
-        ed.forEach(Consumer { handler: EventData -> handler.sb.execute(e, handler.idx) })
-    }
-
     companion object {
         var onInteractHandler = ArrayList<EventData>()
-        @EventHandler
-        private fun onInteract(e: PlayerInteractEvent) {
-            onInteractHandler.forEach(Consumer { handler: EventData? -> })
-        }
+        var onExplosionPrimeHandler = ArrayList<EventData>()
+    }
+
+    @EventHandler
+    private fun onInteract(e: PlayerInteractEvent) {
+        eventExecutor(onInteractHandler, e)
+    }
+
+    @EventHandler
+    private fun onExplosionPrime(e: ExplosionPrimeEvent) {
+        eventExecutor(onExplosionPrimeHandler, e)
+    }
+
+    private fun eventExecutor(ed: ArrayList<EventData>, e: Event?) {
+        ed.forEach { it.sb.execute(e, it.idx) }
     }
 }
