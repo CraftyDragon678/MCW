fe6933bad5603294c4ba91454a60a6f836a63de6
CraftyDragon678 <craftydragon678@gmail.com>
2020-01-18 14:45:05 +0900

    add DurationTimer function

--- src/main/kotlin/com/cragon/mcw/helper/DurationTimer.kt
+++ src/main/kotlin/com/cragon/mcw/helper/DurationTimer.kt
@@ -5,12 +5,18 @@ import com.cragon.mcw.manager.TimerBase
 
 public final class DurationTimer(val sb: SkillBase, val ctimer: CoolDownTimer) : TimerBase() {
     override fun eventStartTimer() {
+        sb.durationStart()
     }
 
     override fun eventRunningTimer(i: Int) {
     }
 
     override fun eventEndTimer() {
+        sb.durationEnd()
+        ctimer.startTimer(sb.coolTime, false)
     }
 
+    override fun finalEventEndTimer() {
+        sb.finalDurationEnd()
+    }
 }
